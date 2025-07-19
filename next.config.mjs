45d8062b23be683730c9b90f1cd17bbf5ebd067b
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-07-19 11:40:51 +0530

    done with the errors now we will what will happen

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -10,27 +8,4 @@ const nextConfig = {
   },
 };
 
-const sentryOptions = {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: "zuhaibs-world",
-  project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors
-  automaticVercelMonitors: true,
-};
-
-export default withSentryConfig(nextConfig, sentryOptions);
+export default nextConfig;
